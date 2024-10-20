be8cc42abb39c0ec745d8a255d84176501a8d559
wissam_6 <wissammerhi@abv.bg>
2024-10-20 10:43:46 +0200

    dependency update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.example {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
 
     opens com.example to javafx.fxml;
+
     exports com.example;
 }
